670bd21b1a59b84f8c9e71a2a76e4fa3a5a14128
Jan-Lukas Schröder <st177404@stud.uni-stuttgart.de>
2025-12-16 09:45:16 +0100

    export binary rendering into custom function

---
+++ vleo_cpp_shader/src/BinaryRenderer.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int BinaryRenderer(float vertecies[],size_t lenVertices, unsigned int triangleIDs[], size_t lenTriangleIDs, bool shaded[], size_t lenShaded,
+					float windX, float windY, float windZ);
